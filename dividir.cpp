f5badb26549b80604cd7c00d8ac39ef64272bd3d
FabianaPadillaF <78805156+FabianaPadillaF@users.noreply.github.com>
2021-05-23 16:48:23 -0400

    Create dividir.cpp

---
+++ dividir.cpp
@@ -0,0 +1,37 @@
+# incluir  " StdAfx.h "
+# include  " dividir.h "
+
+
+
+dividir :: dividir ( vacío )
+{
+}
+int  dividir :: Get_a ()
+{
+ return a;
+}
+int  dividir :: Get_b ()
+{
+ volver b;
+}
+flotar  dividir :: Get_s ()
+{
+ return s;
+}
+void  dividir :: Set_a ( int x)
+{
+ a = x;
+}
+void  dividir :: Set_b ( int y)
+{
+ b = y;
+}
+void  dividir :: Set_s ( flotador p)
+{
+ s = p;
+}
+flotar  dividir :: Calcular ()
+{
+	s = a / b;
+	return s;
+}

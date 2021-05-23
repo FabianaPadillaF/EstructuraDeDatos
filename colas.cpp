47c0868f1bb82d43d8dc1827e82bb193aa3f29e0
FabianaPadillaF <78805156+FabianaPadillaF@users.noreply.github.com>
2021-05-23 16:56:34 -0400

    Create colas.cpp

---
+++ colas.cpp
@@ -0,0 +1,59 @@
+# incluye  < iostream >
+# incluye  " conio.h "
+# incluye  < stdlib.h >
+
+usando el  espacio de nombres  std ;
+
+struct  Nodo {
+	int dato;
+	Nodo * siguiente;
+
+};
+void  insertarCola (Nodo * &, Nodo * &, int );
+bool  cola_vacia (Nodo *);
+
+int  main ()
+{
+	Nodo * frente = NULL ;
+	Nodo * fin = NULL ;
+
+	int dato;
+
+	cout << " Digite un numero: " ;
+	cin >> dato;
+	insertarCola (frente, fin, dato);
+	cout << " Digite un numero: " ;
+	cin >> dato;
+	insertarCola (frente, fin, dato);
+	cout << " Digite un numero: " ;
+	cin >> dato;
+	insertarCola (frente, fin, dato);
+	// getch ();
+	return  0 ;
+}
+void  insertarCola (Nodo * & frente, Nodo * & fin, int n) {
+	Nodo * nuevo_nodo = nuevo  Nodo ();
+
+	nuevo_nodo-> dato = n;
+	nuevo_nodo-> siguiente = NULL ;
+	
+	if ( cola_vacia (frente)) {
+		frente = nuevo_nodo;
+	
+	}
+	else {
+		fin-> siguiente = nuevo_nodo;
+	}
+	fin = nuevo_nodo;
+	cout << " Elemento " << n << " insertado una cola correctamente \ n " ;
+}
+bool  cola_vacia (Nodo * frente) {
+	return (frente == NULL )? verdadero : falso ;
+	if (frente == NULL ) {
+		devuelve  verdadero ;
+	}
+	else {
+		devolver  falso ;
+	}
+	
+}

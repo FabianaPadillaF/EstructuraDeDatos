f9cf34275a2e02697d74d8b9d3a59dd494774fdd
FabianaPadillaF <78805156+FabianaPadillaF@users.noreply.github.com>
2021-05-23 16:46:46 -0400

    Create dividir.h

---
+++ dividir.h
@@ -0,0 +1,18 @@
+# pragma once
+clase  ref dividir
+{
+privado: 
+	int a;
+	int b;
+	flotar s;
+público:
+	dividir ( vacío );
+	int  Get_a ();
+	int  Get_b ();
+	flotar  Get_s ();
+	void  Set_a ( int x);
+	void  Set_b ( int y);
+	void  Set_s ( flotador p);
+
+	flotar  Calcular ();
+};
